7bfb7b7e0cdd127c5da69ee9f609e46e53958d33
Marcin Suski <114236312+marcinsuski@users.noreply.github.com>
2023-01-02 20:55:03 +0100

    homepage done

--- src/components/Coinstable.js
+++ src/components/Coinstable.js
@@ -4,6 +4,7 @@ import { Container } from "@mui/system";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import {
     LinearProgress,
+    Pagination,
     Table,
     TableBody,
     TableCell,
@@ -17,16 +18,38 @@ import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import classes from "./Coinstable.module.css";
 import { useNavigate } from "react-router-dom";
-
 import { numberWithCommas } from "./Banner/Carousel";
 
+
+
+  const darkTheme = createTheme({
+    palette: {
+        primary: {
+            main: "#fff",
+        },
+        secondary: {
+            main: "#ffb300",
+        },
+        accent: {
+            main: "#ffb300",
+        },
+        mode: "dark",
+    },
+});
+
+
 const Coinstable = () => {
-    let navigate = useNavigate();   
+
+
+    let navigate = useNavigate();
     const { symbol } = useContext(CryptoContext);
 
     const [coins, setCoins] = useState([]);
     const [loading, setLoading] = useState(false);
     const [search, setSearch] = useState("");
+    const [page, setPage] = useState(1);
+
+
 
     const { currency } = useContext(CryptoContext);
 
@@ -40,15 +63,6 @@ const Coinstable = () => {
     useEffect(() => {
         fetchCoins();
     }, [currency]);
-
-    const darkTheme = createTheme({
-        palette: {
-            primary: {
-                main: "#fff",
-            },
-            mode: "dark",
-        },
-    });
 
     const handleSearch = () => {
         return coins.filter(
@@ -71,9 +85,9 @@ const Coinstable = () => {
                     onChange={(e) => setSearch(e.target.value)}
                 />
                 <TableContainer>
-                    {loading ? (
+                         {loading ? (
                         <LinearProgress className={classes.table__loading} />
-                    ) : (
+                        ) : (
                         <Table>
                             <TableHead className={classes.table__tablehead}>
                                 <TableRow>
@@ -96,58 +110,105 @@ const Coinstable = () => {
                                 </TableRow>
                             </TableHead>
                             <TableBody>
-                                {handleSearch().map((row) => {
-                                    const profit =
-                                        row.price_change_percentage_24h > 0;
+                                {handleSearch()
+                                    .slice(
+                                        (page - 1) * 10,
+                                        (page - 1) * 10 + 10
+                                    )
+                                    .map((row) => {
+                                        const profit =
+                                            row.price_change_percentage_24h > 0;
 
-                                    return (
-                                        <TableRow
-                                        className={classes.table__row}
-                                            onClick={() =>
-                                                (navigate(`/coins/$${row.id}`))
-                                            }
-                                            key={row.name}
-                                        >
-                                            <TableCell
-                                                component="th"
-                                                scope="row"
-                                                className={classes.table__cell}
+                                        return (
+                                            <TableRow
+                                                className={classes.table__row}
+                                                onClick={() =>
+                                                    navigate(
+                                                        `/coins/$${row.id}`
+                                                    )
+                                                }
+                                                key={row.name}
                                             >
-                                                <img
-                                                    src={row?.image}
-                                                    alt={row.name}
-                                                ></img>
-                                                <div className={classes.table__cell_div}>
-                                                    <span className={classes.table__cell_symbol}>{row.symbol}</span>
-                                                    <span className={classes.table__cell_name}>{row.name}</span>
-                                                </div>
-                                            </TableCell>
-                                            <TableCell align="right">
-                                                {symbol}{" "}
-                                                {numberWithCommas(row.current_price.toFixed(2))}
-                                            </TableCell>
-                                            <TableCell align="right" 
-                                                style={{
-                                                    color: profit > 0 ? "rgb(14,203,129)" : "red",
-                                                    fontWeight: 500,
-                                                }}
+                                                <TableCell
+                                                    component="th"
+                                                    scope="row"
+                                                    className={
+                                                        classes.table__cell
+                                                    }
+                                                >
+                                                    <img
+                                                        src={row?.image}
+                                                        alt={row.name}
+                                                    ></img>
+                                                    <div
+                                                        className={
+                                                            classes.table__cell_div
+                                                        }
+                                                    >
+                                                        <span
+                                                            className={
+                                                                classes.table__cell_symbol
+                                                            }
+                                                        >
+                                                            {row.symbol}
+                                                        </span>
+                                                        <span
+                                                            className={
+                                                                classes.table__cell_name
+                                                            }
+                                                        >
+                                                            {row.name}
+                                                        </span>
+                                                    </div>
+                                                </TableCell>
+                                                <TableCell align="right">
+                                                    {symbol}{" "}
+                                                    {numberWithCommas(
+                                                        row.current_price.toFixed(
+                                                            2
+                                                        )
+                                                    )}
+                                                </TableCell>
+                                                <TableCell
+                                                    align="right"
+                                                    style={{
+                                                        color:
+                                                            profit > 0
+                                                                ? "rgb(14,203,129)"
+                                                                : "red",
+                                                        fontWeight: 500,
+                                                    }}
                                                 >
                                                     {profit && "+"}
-                                                    {row.price_change_percentage_24h.toFixed(2)}%
-                                            </TableCell>
-                                            <TableCell align="right">
-                                                {symbol}{" "}
-                                                {numberWithCommas(row.market_cap.toString())}
-                                                 
-                                            </TableCell>
-                                        </TableRow>
-                                    );
-                                })}
+                                                    {row.price_change_percentage_24h.toFixed(
+                                                        2
+                                                    )}
+                                                    %
+                                                </TableCell>
+                                                <TableCell align="right">
+                                                    {symbol}{" "}
+                                                    {numberWithCommas(
+                                                        row.market_cap.toString()
+                                                    )}
+                                                </TableCell>
+                                            </TableRow>
+                                        );
+                                    })}
                             </TableBody>
                         </Table>
+
                     )}
                 </TableContainer>
-            </Container>
+                    <Pagination 
+                    color="secondary"
+                    className={classes.pagination}
+                    count={(handleSearch()?.length/10).toFixed(0)} 
+                    onChange={(_, value)=> {
+                        setPage(value);
+                        window.scroll(0, 450);
+                    }}
+                    />
+                </Container>
         </ThemeProvider>
     );
 };

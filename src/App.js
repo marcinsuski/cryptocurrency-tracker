5766274bc00b219f02076ff5faca84c96a95c765
Marcin Suski <114236312+marcinsuski@users.noreply.github.com>
2023-01-14 17:57:18 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import classes from "./App.module.css";
 import Header from "./components/Header";
 import Homepage from "./Pages/Homepage";
 import CoinPage from "./Pages/CoinPage";
-import StyledEngineProvider from '@mui/material/StyledEngineProvider';
+import StyledEngineProvider from "@mui/material/StyledEngineProvider";
 import { CryptoContext } from "./CryptoContext";
 import { useEffect, useState } from "react";
 
-
 function App() {
-
     const [currency, setCurrency] = useState("USD");
     const [symbol, setSymbol] = useState("$");
 
@@ -24,15 +22,19 @@ function App() {
     return (
         <CryptoContext.Provider value={{ currency, setCurrency, symbol }}>
             <StyledEngineProvider injectFirst>
-            <BrowserRouter>
-                <div className={classes.App}>
-                    <Header />
-                    <Routes>
-                        <Route path="/" exact element={<Homepage />} />
-                        <Route path="/coins/:id" element={<CoinPage />} />
-                    </Routes>
-                </div>
-            </BrowserRouter>
+                <BrowserRouter>
+                    <div className={classes.App}>
+                        <Header />
+                        <Routes>
+                            <Route path="/" exact element={<Homepage />}>
+                                <Route
+                                    path="/coins/:id"
+                                    element={<CoinPage />}
+                                />
+                            </Route>
+                        </Routes>
+                    </div>
+                </BrowserRouter>
             </StyledEngineProvider>
         </CryptoContext.Provider>
     );
